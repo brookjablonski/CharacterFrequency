5151dfc1e9fdc347f29d18907c6c1cf8d0740aa1
brookjabo <bjablonski@unomaha.edu>
2018-09-20 15:16:01 -0500

    Added JavaDoc

--- src/main/java/org/bjablonski/CharacterFrequency.java
+++ src/main/java/org/bjablonski/CharacterFrequency.java
@@ -6,6 +6,12 @@ import java.util.Optional;
 
 public class CharacterFrequency {
 
+    /**
+     * This method takes in a string and counts the frequency of the alphabetic characters
+     * in the string, case insensitive, and returns a Map<Character, Long> of those character frequencies.
+     * @param String stringToBeCounted
+     * @return Map<Character, Long> characterFrequency
+     */
     public Map<Character, Long> countCharacterFrequency(String stringToBeCounted) {
         HashMap<Character, Long> characterFrequency = new HashMap<Character, Long>();
         stringToBeCounted = stringToBeCounted.toLowerCase();
