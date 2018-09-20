19c53c54280841089e812fb166bd804d9f24f793
brookjabo <bjablonski@unomaha.edu>
2018-09-20 15:10:10 -0500

    Added Optionals

--- src/main/java/org/bjablonski/CharacterFrequency.java
+++ src/main/java/org/bjablonski/CharacterFrequency.java
@@ -2,20 +2,21 @@ package org.bjablonski;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class CharacterFrequency {
 
     public Map<Character, Long> countCharacterFrequency(String stringToBeCounted) {
         HashMap<Character, Long> characterFrequency = new HashMap<Character, Long>();
         stringToBeCounted = stringToBeCounted.toLowerCase();
+
         for(char characterToCheck : stringToBeCounted.toCharArray()) {
             if(Character.isLetter(characterToCheck)){
-                Long frequency = characterFrequency.get(characterToCheck);
-                if(frequency == null){
-                    characterFrequency.put(characterToCheck, 1L);
-                } else {
-                    characterFrequency.put(characterToCheck, new Long(frequency + 1));
-                }
+                Long frequency = Optional.ofNullable(
+                        characterFrequency.get(characterToCheck))
+                        .orElse(0L);
+
+                    characterFrequency.put(characterToCheck, frequency + 1);
             }
         }
         return characterFrequency;
